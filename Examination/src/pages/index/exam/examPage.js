3e130793ccf88b7b6277a90e15d4758a419f52e7
hmm <HMM5412@163.com>
2019-07-10 10:20:16 +0800

    主页侧栏

--- Examination/src/pages/index/exam/examPage.js
+++ Examination/src/pages/index/exam/examPage.js
@@ -8,63 +8,86 @@ function examPage(props){
     return(
         <div className={styles.exam}>
           <Layout style={{ minHeight: '100vh' }}>
-             <Sider>
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="pie-chart" />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop" />
-              <span>Option 2</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="user" />
-                  <span>User</span>
-                </span>
-              }
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="team" />
-                  <span>Team</span>
-                </span>
-              }
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-              <Icon type="file" />
-              <span>File</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+          {/* 左侧导航 */}
+            <Sider>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                     <SubMenu
+                        key="sub1"
+                        title={
+                            <span>
+                            <Icon type="fund" />
+                            <span>试题管理</span>
+                            </span>
+                        }
+                    >
+                        <Menu.Item key="1">添加试题</Menu.Item>
+                        <Menu.Item key="2">试题分类</Menu.Item>
+                        <Menu.Item key="3">查看试题</Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                        key="sub2"
+                        title={
+                            <span>
+                            <Icon type="user" />
+                            <span>用户管理</span>
+                            </span>
+                        }
+                    >
+                        <Menu.Item key="4">添加用户</Menu.Item>
+                        <Menu.Item key="5">用户展示</Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                        key="sub3"
+                        title={
+                            <span>
+                            <Icon type="read" />
+                            <span>考试管理</span>
+                            </span>
+                        }
+                    >
+                        <Menu.Item key="6">添加考试</Menu.Item>
+                        <Menu.Item key="7">试卷列表</Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                        key="sub4"
+                        title={
+                            <span>
+                            <Icon type="container" />
+                            <span>班级管理</span>
+                            </span>
+                        }
+                    >
+                        <Menu.Item key="8">班级管理</Menu.Item>
+                        <Menu.Item key="9">教室管理</Menu.Item>
+                        <Menu.Item key="10">学生管理</Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                        key="sub5"
+                        title={
+                            <span>
+                            <Icon type="container" />
+                            <span>阅卷管理</span>
+                            </span>
+                        }
+                    >
+                        <Menu.Item key="11">待批班级</Menu.Item>
+                    </SubMenu>
+                </Menu>
+            </Sider>
+            {/* 右侧详情 */}
             <Layout>
-            <Header style={{ background: '#fff', padding: 0 }} />
-            <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                <Header style={{ background: '#fff', padding: 0 }} />
+                <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
             </Layout>
-      </Layout>
-            
-        
-        
+          </Layout>
         </div>
     )
 }
