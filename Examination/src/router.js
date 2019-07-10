529cc0586d0b0fe1241aadd6347fac5a0d5a65ff
hmm <HMM5412@163.com>
2019-07-10 08:45:43 +0800

    添加了跳路由进主页

--- Examination/src/router.js
+++ Examination/src/router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch} from 'dva/router';
 import LoginPage from "./pages/login/IndexPage";
+import ExamPage from "./pages/index/exam/examPage"
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/login" component={LoginPage} />
-     
+        <Route path="/exam" component={ExamPage} />
       </Switch>
     </Router>
   );
