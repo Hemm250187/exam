18486841b42aec4173255f9d7d2a6eeefa200223
hmm <HMM5412@163.com>
2019-07-11 15:23:07 +0800

    查看试题数据获取完毕

---
+++ Examination/src/models/exam.js
@@ -0,0 +1,90 @@
+import {exam,examType,subject,getQuestionsType} from '../services/index'
+import {getToken} from "../utils/index"
+import {routerRedux} from "dva/router"
+export default {
+  // 命名空间
+  namespace: 'exam',
+
+  // 模块的状态
+  state: {
+    exam:[],
+    examtype:[],
+    subject:[],
+    questions:[]
+  },
+  //订阅
+  subscriptions: {
+    setup({ dispatch, history }) {  // eslint-disable-line
+      //  console.log("dispatch...",dispatch)
+      //  console.log("history",history)
+      return history.listen(({pathname})=>{
+        //判断去的页面是否是登录页
+          if(pathname.indexOf("/login")===-1){
+            //判断是否有登录态  
+            if(!getToken()){
+              //没有登陆态
+              dispatch(routerRedux.replace({
+                pathname:`/login`,
+                search:`?redirect=${encodeURIComponent(pathname)}`
+              }))
+            }
+          }else{
+            //是登录页
+            if(getToken()){
+              dispatch(routerRedux.replace({pathname:"/exam"}))
+            }
+          }
+      })
+    },
+  },
+  // 异步操作
+  effects: {
+    *exam({payload},{call,put}){
+      let data= yield call(exam);
+      //console.log("exam...",data)
+      yield put({
+        type:"findexam",
+        payload:data.data
+      })
+    },
+    *type({},{call,put}){
+      let list = yield call(examType);
+      yield put({
+        type:"examtype",
+        payload:list.data
+      })
+    },
+    *subjecttype({},{call,put}){
+      let list = yield call(subject);
+      yield put({
+        type:"findsubject",
+        payload:list.data
+      })
+    },
+    *questionsTypes({},{call,put}){
+      let list = yield call(getQuestionsType);
+      console.log(list)
+      yield put({
+        type:"questionsType",
+        payload:list.data
+      })
+    }
+  },
+
+  // 同步操作
+  reducers: {
+    findexam(state,action){
+      return {...state,exam:action.payload}
+    },
+    examtype(state,action){
+      return {...state,examtype:action.payload}
+    },
+    findsubject(state,action){
+      return {...state,subject:action.payload}
+    },
+    questionsType(state,action){
+      return {...state,questions:action.payload}
+    }
+  },
+
+};
--- Examination/src/pages/index/exam/questions/checkquestion.js
+++ Examination/src/pages/index/exam/questions/checkquestion.js
@@ -1,106 +1,100 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import {connect} from "dva";
 import "../css/examPage.css"
-import { Checkbox,Select,Button,Tag } from 'antd';
-const CheckboxGroup = Checkbox.Group;
-const { Option } = Select;
-const plainOptions = ['javaScript上', 'javaScript下',"模块化开发","移动端开发","node基础","组件化开发(vue)","渐进式开发(react)","项目实战","javaScript高级","node高级"];
-const defaultCheckedList = [];
+import { Select,Button,Tag} from 'antd';
 
-class checkquestion extends Component {
-    state = {
-        checkedList: defaultCheckedList,
-        indeterminate: true,
-        checkAll: false,
-      };
-    componentDidMount(){
-        console.log(this.props)
+const { Option } = Select;
+
+function checkquestion(props) {
+    let {exam,examtype,subject,questions}=props;
+    useEffect(()=>{
+       props.getQuestion(),
+       props.getExamType(),
+       props.getsubject(),
+       props.getQuestionsType()
+    },[])
+   
+    let subjectdelog=()=>{
+        let {history}=props;
+        history.push({pathname:"/exam/details"})
     }
-      onChange = checkedList => {
-        this.setState({
-          checkedList,
-          indeterminate: !!checkedList.length && checkedList.length < plainOptions.length,
-          checkAll: checkedList.length === plainOptions.length,
-        });
-      };
-    
-      onCheckAllChange = e => {
-        this.setState({
-          checkedList: e.target.checked ? plainOptions : [],
-          indeterminate: false,
-          checkAll: e.target.checked,
-        });
-      };
-      subjectdelog(){
-          let {history}=this.props;
-          history.push({pathname:"/exam/details"})
-      }
-    render() {
-      
-        return (
-            <div className="seequest">
-              <h2>查看试题</h2>
-              <div className="navquest">
+    return (
+        <div className="seequest">
+            <h2>查看试题</h2>
+            <div className="navquest">
                 <div className="course">
                     <div className="coursetype">课程类型：</div>
                     <div className="listcourse">
-                        <div>
-                            <Checkbox
-                                indeterminate={this.state.indeterminate}
-                                onChange={this.onCheckAllChange}
-                                checked={this.state.checkAll}
-                            >
-                               All
-                            </Checkbox>
-                        </div>
-                        <CheckboxGroup
-                        options={plainOptions}
-                        value={this.state.checkedList}
-                        onChange={this.onChange}
-                        />
+                        <span>All</span>
+                        {subject.map(item=><span key={item.subject_id}>{item.subject_text}</span>)}
                     </div>
                 </div>
                 <div className="other">
                     <div className="othertype">
                     <span>考试类型</span>：
                         <Select defaultValue="周考一" style={{ width: 120 }} >
-                            <Option value="周考一">周考一</Option>
-                            <Option value="周考二">周考二</Option>
-                            <Option value="周考三" >
-                            周考三
-                            </Option>
-                            <Option value="月考">月考</Option>
+                        {examtype&&examtype.map((item)=><Option value={item.exam_name} key={item.exam_id}>{item.exam_name}</Option>)}
                         </Select>
                     </div>
                     <div className="othertype">
                     <span>题目类型：</span>
                         <Select defaultValue="简答题" style={{ width: 120 }} >
-                            <Option value="简答题">简答题</Option>
-                            <Option value="代码阅读题">代码阅读题</Option>
-                            <Option value="代码补全" >代码补全</Option>
-                            <Option value="修改BUG">修改BUG</Option>
-                            <Option value="手写代码">手写代码</Option>
+                        {questions&&questions.map(item=><Option value={item.questions_type_text} key={item.questions_type_id}>{item.questions_type_text}</Option>)}
                         </Select>
                     </div>
                     <Button type="primary" icon="search">查询</Button>
                 </div>
-              </div>
-              <div className="listquest">
-                 <div className="subject">
-                   <div className="listsubject" onClick={this.subjectdelog.bind(this)}>
-                     <h4>机器人归位</h4>
-                     <div className="labels">
-                        <Tag color="gold">代码补全</Tag>
-                        <Tag color="lime">javaScript上</Tag>
-                        <Tag color="green">周考一</Tag>
-                     </div>
-                     <div className="usesubject">uyhjko发布</div>
-                   </div>
-                   <div className="edit">编辑</div>
-                 </div>
-              </div>
             </div>
-        )
+            <div className="listquest">
+                {exam&&exam.map((item,index)=>
+                    <div className="subject" key={index}>
+                        <div className="listsubject" onClick={()=>subjectdelog()}>
+                            <h4>{item.title}</h4>
+                            <div className="labels">
+                                <Tag color="gold">{item.questions_type_text}</Tag>
+                                <Tag color="lime">{item.subject_text}</Tag>
+                                <Tag color="green">{item.exam_name}</Tag>
+                            </div>
+                            <div className="usesubject">{item.user_name}发布</div>
+                        </div>
+                        <div className="edit">编辑</div>
+                    </div>
+                )}
+            </div>
+        </div>
+    ) 
+}
+const mapStateToProps = (state) => {
+    return {
+        ...state.exam 
+    }
+ }
+const mapDispatchToProps = dispatch => {
+    return {
+      //获取所有的试题
+       getQuestion: () => {
+           dispatch({
+               type:"exam/exam",
+           })
+        },
+      //获取所有的考试类型
+      getExamType:()=>{
+          dispatch({
+              type:"exam/type"
+          })
+      },
+      //获取所有的课程
+      getsubject:()=>{
+          dispatch({
+              type:"exam/subjecttype"
+          })
+      },
+      //获取所有的试题类型
+      getQuestionsType:()=>{
+          dispatch({
+              type:"exam/questionsTypes"
+          })
+      }
     }
 }
-export default connect()(checkquestion)
+export default connect(mapStateToProps,mapDispatchToProps)(checkquestion)
