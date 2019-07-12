cb88ef098945592f5665eb091efa04656e289367
hmm <HMM5412@163.com>
2019-07-12 21:19:03 +0800

    编辑初步

--- Examination/src/pages/index/exam/questions/addquestions.js
+++ Examination/src/pages/index/exam/questions/addquestions.js
@@ -1,11 +1,144 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import {connect} from 'dva'
+import Editor from 'for-editor'
+//import { Router, Route, Switch ,Redirect} from 'dva/router';
+import "../css/examPage.css"
+import { Input ,Form,Button, Select} from 'antd';
 
-export default class addquestions extends Component {
-    render() {
-        return (
+
+//const { Option} = Select;
+function AddQuestion(props) {
+    console.log(props)
+
+    useEffect(() => {
+        props.getQuestionTypes()
+        props.getsubject()
+        props.Allquestion()
+      }, [])
+
+  const {getFieldDecorator}=props.form;
+
+ return (
+       <Form className="login-form">
+            <div className="addPage">
+                <h4>添加试题</h4>
+                
+                <div className="mains">
+                <div>
+                    <p>题目信息</p>
+                    <Form.Item>
+                        <p>题干</p>
+                                {getFieldDecorator('titleText', {
+                                    rules: [{ required: true, message: '标题不能为空!' }],
+                                })(
+                                    <Input
+                                    placeholder="请输入题目标题，不能超过20字"
+                                    />,
+                                )}
+                    </Form.Item>
+                </div>
+               <div className="themList">
+                   <p>题目主题</p>
+                  <div className="editfor">
+                    <Form.Item>
+                                {getFieldDecorator('value', {
+                                    rules: [{ required: true, message: "答案信息必填" }],
+                                    initialValue: '',
+                                })(
+                                    <Editor height='auto'/>
+                                )}
+                    </Form.Item>
+                  </div>
+                  <div className="themList">
+                         <p>请选择考试类型：</p>
+                         {/* <Form.Item>
+                                {getFieldDecorator('exam_id', {
+                                    rules: [{ required: true, message: "题目类型必选" }],
+                                    initialValue: "请选择题目类型"
+                                })(
+                                <Select style={{ width: 120 }} initialValue="简答题">
+                                   {props.TypeList.map(item=><Option key={item.questions_type_id} value={item.questions_type_text}>{item.questions_type_text}</Option>)}
+                                </Select>
+                                )}
+                        </Form.Item> */}
+                  </div>
+                  <div className="themList">   
+              <p>请选择课程类型：</p>
+                     {/* <Form.Item>
+                        {getFieldDecorator('subject_id', {
+                            rules: [{ required: true, message: "题目类型必选" }],
+                            initialValue: "请选择题目类型"
+                        })(
+                            <Select  style={{ width: 120 }}>
+                           {props.subjects.map(item=><Option key={item.subject_id} value={item.subject_text}>{item.subject_text}</Option>)}
+                            </Select>
+                        )}
+                        </Form.Item> */}
+       </div>
+       <div className="themList">     
+              <p>请选择题目类型：</p>
+              {/* <Form.Item>
+                    {getFieldDecorator('questions_type_id', {
+                        rules: [{ required: true, message: "题目类型必选" }],
+                        initialValue: "简答题"
+                    })(
+                        <Select  style={{ width: 120 }}>
+                           {props.insertList.map(item=><Option key={item.questions_type_id} value={item.questions_type_text}>{item.questions_type_text}</Option>)}
+                        </Select>
+                    )}
+             </Form.Item> */}
+       </div>
+                 <p>答案信息</p>
+                 {/* <Form.Item>
+                            {getFieldDecorator('valueowen', {
+                                rules: [{ required: true, message: "答案信息必填" }],
+                                initialValue: '',
+                            })(
+                                <Editor height='auto'/>
+                            )}
+                 </Form.Item> */}
             <div>
-                添加试题
+            <Button type="primary" htmlType="submit" >提交</Button>
+            </div> 
+               </div>
+                </div>
             </div>
+            </Form>
         )
+}
+
+const mapStateToProps=(state)=>{
+    console.log(state)
+    return {
+        // ...state.power
+        ...state.exam 
     }
 }
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        //获取所有的试题类型
+        getQuestionTypes:()=>{
+            dispatch({
+                // type:"power/type"
+                type:"exam/questionsTypes"
+            })
+        },
+       //获取所有的课程
+        getsubject:()=>{
+            dispatch({
+                // type:"power/getsubject"
+                type:"exam/subjecttype"
+            })
+        },
+         //获取所有的试题
+        Allquestion:()=>{
+            dispatch({
+                // type:"power/Allquestion"
+                type:"exam/exam"
+            })
+        }
+
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Form.create()(AddQuestion))
--- Examination/src/pages/index/exam/questions/editQuestion.js
+++ Examination/src/pages/index/exam/questions/editQuestion.js
@@ -1,14 +1,114 @@
-import React from 'react'
-import "../css/examPage.css"
-import {Tag} from 'antd';
-function detail(props){
-    let exam = props.location.params.exam;
-    console.log(exam) 
-    return (
-        <div className="edit_exam">
-            <h2>修改试题</h2>
-            
-        </div>
-    )
+import React, { useEffect, useState } from 'react'
+import {connect} from 'dva'
+import Editor from 'for-editor'
+//import { Router, Route, Switch ,Redirect} from 'dva/router';
+import "../css/addQuestion.css"
+import { Input ,Form, Button,Select,Modal} from 'antd';
+
+const { Option} = Select;
+function AddQuestion(props) {
+    let {examtype,subject,questions}=props;
+    console.log(props.location.params.exam)
+    let editexam = props.location.params.exam;
+    useEffect(() => {
+        props.getQuestionTypes()
+        props.getsubject()
+        props.getExamType()
+      }, [])
+
+   const {getFieldDecorator}=props.form;
+
+   return (
+    <Form className="edit_exam">
+        <h2>编辑试题</h2>
+        <div className="edit_main">
+            <div>
+                <p>题目信息</p>
+                <Form.Item>
+                    <p>题干</p>
+                    <Input value={editexam.title} placeholder="请输入题目标题，不超过20个字"/>
+                    {/* {getFieldDecorator('titleText', {
+                        rules: [{ required: true, message: '标题不能为空!' }],
+                        })(
+                        <Input
+                         placeholder={editexam.title}
+                        />,
+                    )} */}
+                </Form.Item>
+            </div>
+            <p>题目主题</p>
+            <Form.Item>
+             <Editor height='auto' value={editexam.questions_stem}/>
+                {/* {getFieldDecorator('value', {
+                rules: [{ required: true, message: "答案信息必填" }],
+                initialValue: '',
+                })(
+                <Editor height='auto' value={editexam.questions_stem}/>
+                )} */}
+            </Form.Item>
+
+            <div className="themList">
+                <p>请选择考试类型：</p>
+                <Select defaultValue={editexam.exam_name} style={{ width: 120 }} >
+                    {examtype&&examtype.map((item)=><Option value={item.exam_name} key={item.exam_id}>{item.exam_name}</Option>)}
+                </Select>
+            </div>
+            <div className="themList">   
+                <p>请选择课程类型：</p>
+                <Select defaultValue="js" style={{ width: 120 }} >
+                    {subject&&subject.map((item)=><Option value={item.subject_text} key={item.subject_id}>{item.subject_text}</Option>)}
+                </Select>
+            </div>
+            <div className="themList">     
+                <p>请选择题目类型：</p>
+                <Select defaultValue="简答题" style={{ width: 120 }} >
+                    {questions&&questions.map(item=><Option  value={item.questions_type_text} key={item.questions_type_id}>{item.questions_type_text}</Option>)}
+                </Select>
+            </div>
+            <p>答案信息</p>
+            <Editor height='auto' value={editexam.questions_answer}/>
+                {/* <Form.Item>
+                    
+                {getFieldDecorator('valueowen', {
+                rules: [{ required: true, message: "答案信息必填" }],
+                initialValue: '',
+                })(
+                <Editor height='auto' value={editexam.questions_answer}/>
+                )}
+                
+                </Form.Item> */}
+            <Button type="primary" htmlType="submit" >提交</Button>
+        </div> 
+    </Form>
+        )
 }
-export default detail
+
+const mapStateToProps=(state)=>{   
+    return {
+        ...state.exam 
+    }
+}
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        //获取所有的试题类型
+        getQuestionTypes:()=>{
+            dispatch({
+                type:"exam/questionsTypes"
+            })
+        },
+        //考试类型
+        getExamType:()=>{
+            dispatch({
+                type:"exam/type"
+            })
+        },
+       //获取所有的课程
+        getsubject:()=>{
+            dispatch({
+                type:"exam/subjecttype"
+            })
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Form.create()(AddQuestion))
